d04714e3b99feb150d07ebd090849ecf163a8664
Leandro Romano <leandroromano.dev@gmail.com>
2019-06-27 22:45:24 -0300

    Initial project commit.

---
+++ Test.GuiaBolsoTests/Test_GuiaBolsoTests.swift
@@ -0,0 +1,34 @@
+//
+//  Test_GuiaBolsoTests.swift
+//  Test.GuiaBolsoTests
+//
+//  Created by Leandro Romano on 27/06/19.
+//  Copyright © 2019 Leandro Romano. All rights reserved.
+//
+
+import XCTest
+@testable import Test_GuiaBolso
+
+class Test_GuiaBolsoTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
